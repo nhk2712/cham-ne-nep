764c7687c2a8043ea0921e6797439bc5d0d9ee49
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-02-12 18:04:52 +0700

    Basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Category from './components/Category';
 
 function App() {
+  const [xepHang, setXepHang] = useState(0);
+  const [dongPhuc, setDongPhuc] = useState(0);
+  const [theHocSinh, setTheHocSinh] = useState(0);
+  const [huyHieuDoan, setHuyHieuDoan] = useState(0);
+  const [dauToc, setDauToc] = useState(0);
+
   return (
-    <div className="App">
+    <div className='App'>
+      <Category title="Xếp hàng" field={xepHang} update={setXepHang} />
+      <Category title="Đồng phục" field={dongPhuc} update={setDongPhuc} />
+      <Category title="Thẻ học sinh" field={theHocSinh} update={setTheHocSinh} />
+      <Category title="Huy hiệu đoàn" field={huyHieuDoan} update={setHuyHieuDoan} />
+      <Category title="Đầu tóc" field={dauToc} update={setDauToc} />
     </div>
   );
 }
